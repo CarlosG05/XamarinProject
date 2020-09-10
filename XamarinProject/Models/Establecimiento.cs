6d7e825eeea6cbace6d6495965d5d34ede15f0c3
FAMILIA <FAMILIA@DESKTOP-SRPR197>
2020-09-09 21:53:12 -0500

    Subir Clase inicial de Establecimiento para el tratimiento de mercados

---
+++ XamarinProject/Models/Establecimiento.cs
@@ -0,0 +1,10 @@
+﻿namespace XamarinProject.Models
+{
+    public class Establecimiento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Articulo { get; set; }
+        public double Valor { get; set; }
+    }
+}
---
+++ XamarinProject/ViewModels/EstablecimientoViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamarinProject.ViewModels
+{
+    public class EstablecimientoViewModel : BaseViewModel
+    {
+        private int id;
+        private string nombre;
+        private string articulo;
+        private double valor;
+        public ICommand ComandoLimpiar { get; set; }
+        public ICommand ComandoEnviar { get; set; }
+
+        public int Id
+        {
+            get => id;
+            set { id = value; OnPropertyChanged(nameof(id)); }
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set { nombre = value; OnPropertyChanged(nameof(nombre)); }
+        }
+
+        public string Articulo
+        {
+            get => articulo;
+            set { articulo = value; OnPropertyChanged(nameof(articulo)); }
+        }
+
+        public double Valor
+        {
+            get => valor;
+            set { valor = value; OnPropertyChanged(nameof(valor)); }
+        }
+
+        #region Metodos
+        void Limpiar()
+        {
+            Nombre = string.Empty;
+            Articulo = string.Empty;
+            Valor = 0.0;
+            
+        }
+
+        async Task Enviar()
+        {
+            try
+            {
+                EmailMessage mensaje = new EmailMessage
+                {
+                    Subject = "¡Nuevo producto!",
+                    Body = $"Producto: {Nombre} \n Artículo: {Articulo }  \nPrecio: {Valor:N2}",
+                    To = new List<string> { "rubenefrain@gmail.com" }
+                };
+                await Email.ComposeAsync(mensaje);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        #endregion
+
+        //Constructor
+        public EstablecimientoViewModel()
+        {
+            ComandoLimpiar = new Command(Limpiar);
+            ComandoEnviar = new Command(async () => await Enviar());
+        }
+
+
+
+    }
+}
