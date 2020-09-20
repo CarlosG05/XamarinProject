497a9108e85b84d8d36a68dc04a0bc1ff77b5412
carlos <carlos_g05@hotmail.es>
2020-09-20 12:28:20 -0500

    inclusion PageCuenta

---
+++ XamarinProject/Paginas/PageAcercaDe.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinProject.Paginas
+{
+    public partial class PageAcercaDe : ContentPage
+    {
+        public PageAcercaDe()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XamarinProject/Paginas/PageCuenta.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XamarinProject.Paginas
+{
+
+    [DesignTimeVisible(false)]
+    [QueryProperty("ResultadoIMC", "resultadoIMC")]
+
+    public partial class PageCuenta : ContentPage
+    {
+        public PageCuenta()
+        {
+            InitializeComponent();
+        }
+
+        public string ResultadoIMC
+        {
+            set { lblResultadoIMC.Text = value; }
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//inicio?mensaje={lblResultadoIMC.Text}");
+        }
+    }
+}
